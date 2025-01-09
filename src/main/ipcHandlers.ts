4416b3e4b9a47702ecf51e9b250a6ef3c8137b2d
Abdul Aziz Ali <azizalialhassan@gmail.com>
2025-01-09 08:30:56 +0000

    minor rewrite

--- src/main/ipcHandlers.ts
+++ src/main/ipcHandlers.ts
@@ -1,8 +1,30 @@
 import { ipcMain } from 'electron'
+import { fetchAll } from 'sqlite-electron'
+// import { Word } from '../preload/interface'
 
 export function initializeIpcHandlers(): void {
   ipcMain.handle('dictionary:query', async (_, word: string) => {
-    return word
+    let resp = (await fetchAll(
+      `
+      SELECT * FROM words 
+      WHERE word = ?
+      `,
+      [word]
+    )) as object[]
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    resp = resp.map((i: any) => {
+      return {
+        ...i,
+        forms: JSON.parse(i.forms),
+        senses: JSON.parse(i.senses),
+        sound: JSON.parse(i.sound),
+        etymology: JSON.parse(i.etymology),
+        templates: JSON.parse(i.templates),
+        categories: JSON.parse(i.categories)
+      }
+    })
+    return resp
   })
   ipcMain.handle('dictionary:history', async () => {
     // return database.getHistory()
--- src/preload/interface.ts
+++ src/preload/interface.ts
@@ -1,5 +1,42 @@
 export interface ElectronAPI {
-  query: (word: string) => Promise<unknown>
+  query: (word: string) => Promise<Word[]>
   openSettings: () => void
   // getHistory: () => Promise<History[]>
+}
+
+export interface Word {
+  categories?: string[]
+  forms?: {
+    form: string
+    tags: string[]
+  }[]
+  head_templates?: {
+    args?: {
+      [key: string]: string
+    }
+    expansion?: string
+    name?: string
+  }[]
+  pos: string
+  etymology_text?: string
+  etymology_templates?: {
+    name?: string
+    args?: {
+      [key: string]: string
+    }
+    expansion?: string
+  }[]
+  hyphenation?: string
+  senses?: {
+    glosses?: string[]
+    tags?: string[]
+    examples?: {
+      text: string
+    }[]
+  }[]
+  sounds?: {
+    ipa?: string
+    tags?: string[]
+  }[]
+  word: string
 }
